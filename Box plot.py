d2c50e5885ba49d604134ecfea837d2769a4436d
Anusha VK <anushavk851@gmail.com>
2026-09-14 23:25:23 +0530

    Implement box plot for age distribution
    Added a box plot to visualize the distribution of ages.

---
+++ Box plot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+#boxplot-used for summarizing the distribution of numerical data using q3,q1,q2,outliers,max and min
+
+ages=[20,2,28,24,25,100,27]
+plt.boxplot(ages)
+plt.show()
